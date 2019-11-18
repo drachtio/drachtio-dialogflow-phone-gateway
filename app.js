0fc1455c31084cf9c77f9385b7123e93753e2119
Dave Horton <daveh@beachdognet.com>
2019-11-18 07:10:36 -0800

    add support for trunks that require us to register

--- app.js
+++ app.js
@@ -5,10 +5,20 @@ const mrf = new Mrf(srf);
 const config = require('config');
 const logger = require('pino')(config.get('logging'));
 const CallSession = require('./lib/call-session');
+const parseExpires = require('./lib/parse-expires');
 
 /* connect to the drachtio server */
 srf.connect(config.get('drachtio'))
-  .on('connect', (err, hp) => logger.info(`connected to sip on ${hp}`))
+  .on('connect', (err, hp) => {
+    logger.info(`connected to sip on ${hp}`);
+    /* some sip trunking providers require us to register in order to receive calls.. */
+    if (config.has('register')  && config.get('register.enabled') === true) {
+      const domain = config.has('register.domain');
+      const proxy = config.has('register.proxy') ? config.get('register.proxy') : null;
+      const auth = config.get('register.auth');
+      registerWithProvider(domain, proxy, auth);
+    }
+  })
   .on('error', (err) => logger.info(err, 'Error connecting'));
 
 /* we want to handle incoming invites */
@@ -23,3 +33,35 @@ srf.invite((req, res) => {
     .on('end', () => logger.debug('dialogflow session ended'));
   callSession.exec();
 });
+
+async function registerWithProvider(domain, proxy, auth) {
+  const uri = `<sip:${auth.username}@${domain}>`;
+  const contact = `<sip:${auth.username}@localhost>`;
+  try {
+    const req = await srf.request({
+      uri: `sip:${domain}`,
+      method: 'REGISTER',
+      proxy,
+      auth,
+      headers: {
+        'To': uri,
+        'From': uri,
+        'Contact': contact,
+        'Expires': 3600
+      }
+    });
+    req.on('response', (res) => {
+      if (res.status === 200) {
+        const duration = parseExpires(res);
+        if (duration) {
+          logger.info('successfully registered with sip provider');
+          setTimeout(registerWithProvider.bind(null, domain, proxy, auth), duration - 10);
+        }
+        else throw new Error('200 OK to REGISTER did not include expires value');
+      }
+      else throw new Error(`Failure registering: ${res.status}`);
+    });
+  } catch (err) {
+    logger.info(err, 'Failed registering with sip provider');
+  }
+}
