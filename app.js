38c09dfebc2e998aa4e5d45dbf8bf41d37f7f3ed
Dave Horton <daveh@beachdognet.com>
2020-02-26 15:42:00 -0500

    simwood changes

--- app.js
+++ app.js
@@ -1,34 +1,24 @@
 const Srf = require('drachtio-srf');
 const srf = new Srf();
-const logger = require('pino')({level: 'info'});
+const logger = require('pino')({level: 'debug'});
 const opts = require('./lib/config')(logger);
 const CallSession = require('./lib/call-session');
 const {initLogging} = require('./lib/middleware')(logger);
-require('./lib/connect-ms')(logger, srf, opts.freeswitch);
+const eventStore = require('./lib/event-store')(logger);
+
+require('./lib/utils/connect-ms')(logger, srf, opts.freeswitch);
 
 /* connect to the drachtio server */
 srf.connect(opts.drachtio)
   .on('connect', async(err, hp) => logger.info(`connected to sip on ${hp}`))
   .on('error', (err) => logger.info(err, 'Error connecting'));
 
-const blacklistUnknownDIDs = (req, res, next) => {
-  if (req.calledNumber !== '+15082139758') {
-    logger.info(`blacklisting ${req.source_address}`);
-    return res.send(403, {
-      headers: {
-        'X-Reason': `detected potential spammer from ${req.source_address}:${req.source_port}`
-      }
-    });
-  }
-  next();
-};
-
 /**
  * install middleware:
  * 1.  If an APIBAN key was provided, block traffic from banned sources
  * 2.  Initialize logger and then connect the incoming call to the freeswitch media server
  */
-const middleware = [blacklistUnknownDIDs, initLogging];
+const middleware = [initLogging];
 if (process.env.APIBAN_KEY) middleware.unshift(require('drachtio-mw-apiban')(process.env.APIBAN_KEY));
 
 srf.use('invite', middleware);
@@ -37,11 +27,16 @@ srf.use('invite', middleware);
 srf.invite((req, res) => {
   const callSession = new CallSession(req, res, opts);
   callSession
-    .on('intent', (intent) => logger.info(intent, 'received intent'))
-    .on('transcription', (transcript) => logger.info(transcript, 'received transcription'))
+    .on('init', (obj) => eventStore.add(callSession.uuid, obj))
+    .on('intent', (intent) => eventStore.addIntent(callSession.uuid, intent))
+    .on('transcription', (transcript) => eventStore.addTranscript(callSession.uuid, transcript))
     .on('end_of_utterance', (evt) => logger.debug(evt, 'received end_of_utterance'))
-    .on('audio', (evt) => logger.debug(`received audio file ${evt.path}`))
+    .on('audio', (evt) => {})
     .on('error', (err) => logger.info(err, 'received error'))
-    .on('end', () => logger.debug('dialogflow session ended'));
+    .on('end', () => eventStore.end(callSession.uuid));
   callSession.exec();
 });
+
+
+// websocket: optional for feeding real-time updates to other apps
+if (opts.ws.port) require('./lib/http')({logger, eventStore, opts: opts.ws});
